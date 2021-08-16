47327aa747e229e7cbba1bed14e7f432850721c0
Josh Ganote <j.ganote0825@gmail.com>
2021-08-15 23:16:05 -0500

    initial format for search destinations

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -3,12 +3,14 @@ import {View, Text} from 'react-native';
 
 import {HomeMap} from '../../components/HomeMap/HomeMap';
 import {CovidMessage} from '../../components/CovidMessage/CovidMessage';
+import {HomeSearch} from '../../components/HomeSearch/HomeSearch';
 
 export const HomeScreen = props => {
   return (
     <View>
       <HomeMap />
       <CovidMessage />
+      <HomeSearch />
     </View>
   );
 };
