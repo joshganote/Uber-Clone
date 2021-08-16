657a6c417bb37f804b7501c161ccce42be59b791
Josh Ganote <j.ganote0825@gmail.com>
2021-08-15 22:40:20 -0500

    chagning file names

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {StatusBar} from 'react-native';
 
-import {HomeScreen} from './src/screens/HomeScreen/index';
+import {HomeScreen} from './src/screens/HomeScreen/HomeScreen';
 
 const App: () => React$Node = () => {
   return (
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-import {HomeMap} from '../../components/HomeMap/index';
-import {CovidMessage} from '../../components/CovidMessage/index';
+import {HomeMap} from '../../components/HomeMap/HomeMap';
+import {CovidMessage} from '../../components/CovidMessage/CovidMessage';
 
 export const HomeScreen = props => {
   return (
